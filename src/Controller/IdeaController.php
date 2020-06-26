b0e488fd8c5ae96ad501636cd43092866705fcc3
jrupin <jean.rupin@gmail.com>
2020-06-26 12:25:27 +0200

    Module5 TP

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -2,30 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Idea;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IdeaController extends Controller
 {
     /**
-     * @Route("/list",name="list" )
+     * @Route("/list",name="idea_list" )
      */
     public function list()
     {
+        $ideaRepo = $this->getDoctrine()->getRepository(Idea::class);
+        $ideas = $ideaRepo->findBy(['isPublished'=>true],['dateCreated'=>'DESC']);
 
-        return $this->render('/default/list.html.twig', [
-            'controller_name' => 'DefaultController',
+
+        return $this->render('/idea/list.html.twig', [
+           "ideas"=>$ideas
         ]);
     }
 
     /**
-     * @Route("/detail/{id}",name="detail")
+     * @Route("/detail/{id}",name="idea_detail")
      */
     public function detail($id)
     {
 
-        return $this->render('/default/detail.html.twig', [
-            'controller_name' => 'DefaultController',
+        $ideaRepo = $this->getDoctrine()->getRepository(Idea::class);
+        $idea= $ideaRepo->find($id);
+        return $this->render('/idea/detail.html.twig', [
+            'idea'=>$idea
         ]);
     }
 }
